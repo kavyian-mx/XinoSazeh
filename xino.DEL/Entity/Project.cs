199230ef21a998feb45488fe8b7a6c124aca9482
Matin <Matin@DESKTOP-QUGOMAD>
2025-11-22 11:17:16 +0330

    add-ui-home-callme

--- xino.DEL/Entity/Project.cs
+++ xino.DEL/Entity/Project.cs
@@ -2,7 +2,7 @@
 
 namespace xino.DEL.Entity;
 
-  public  class Project
+  public  class Project:PageBaseSeo
     {
     public int Id { get; set; }
 
---
+++ xino/Controllers/CallMeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ENDPOINTs.Controllers
+{
+    public class CallMeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
